5429460b647af5ceb236964c9c52222de58d829f
Philippe DeSousa <phil.desousa@gmail.com>
2026-03-17 14:07:21 -0400

    update bucket name

--- constants.go
+++ constants.go
@@ -1,15 +1,15 @@
 package main
 
-// const OriginURL = "http://localhost:5173"
+const OriginURL = "http://localhost:5173"
 
-const OriginURL = "https://d3knrnqllqxltd.cloudfront.net"
+// const OriginURL = "https://d3knrnqllqxltd.cloudfront.net"
 const TableName = "MyrestoSettings"
 const ApiPath = "settings"
 
-// const localMode = true
+const localMode = true
 
-const localMode = false
+// const localMode = false
 
-const bucketName = "myrestosettings-64fc154d"
+const bucketName = "myresto-settings-bucket-1f741eb9"
 
 const settingsFilename = "settings.json"
